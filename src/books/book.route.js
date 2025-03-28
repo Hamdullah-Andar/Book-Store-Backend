037f6dabe0964dd984bb65e6000a7f95d65d6aa4
= <108467703+Hamdullah-Andar@users.noreply.github.com>
2025-03-28 17:02:03 +0430

    Store Book data to database using create-book Route

--- src/books/book.route.js
+++ src/books/book.route.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const Book = require("./book_model");
 const router = express.Router();
 
 // Post a bokk
 router.post("/create-book", async (req, res) => {
-  console.log(req.body);
+  try {
+    const newBook = await Book({ ...req.body });
+    await newBook.save();
+    res
+      .status(200)
+      .send({ message: "Book posted Successfully", book: newBook });
+  } catch (error) {
+    console.error("Error Creating Book", error);
+    res.status(500).send({ message: "Failed to post Book", error: error });
+  }
 });
 
 module.exports = router;

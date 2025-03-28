0a8d171a9f29956d68851d3eb4fad37633aa51d5
= <108467703+Hamdullah-Andar@users.noreply.github.com>
2025-03-28 16:33:48 +0430

    Add Book Schema, Create Book Route, Install cors

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const mongoose = require("mongoose");
 
 const port = process.env.PORT || 5000;
-require('dotenv').config()
+require("dotenv").config();
+
+// Middleware
+app.use(express.json());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+
+// Routes
+const bookRoutes = require("./src/books/book.route");
+app.use("/api/books", bookRoutes);
 
 async function main() {
   await mongoose.connect(process.env.DB_URL);

30af65155a6254751b463159df06dc666e7b0030
= <108467703+Hamdullah-Andar@users.noreply.github.com>
2025-04-09 16:47:43 +0430

    Remove the local Cors link

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ require("dotenv").config();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://k-book-store-frontend.vercel.app", "http://localhost:5173"],
+    origin: ["https://k-book-store-frontend.vercel.app"],
     credentials: true,
   })
 );
